87967e725ac35c6f8db43eb06ca8e2a68e540d64
Rafen Sidiq Anggara <134561537+Rawnreff@users.noreply.github.com>
2025-06-15 17:01:35 +0700

    1

--- app/Http/Controllers/Frontend/RecipeController.php
+++ app/Http/Controllers/Frontend/RecipeController.php
@@ -11,11 +11,17 @@ class RecipeController extends Controller
 {
     public function index(Request $request) {
         $recipes = Recipe::with('galleries');
+        
         if($q = $request->query('search')) {
             $q = str_replace('-', ' ', Str::slug($q));
-
-            $recipes = $recipes->whereRaw('MATCH(title, slug) AGAINST (? IN NATURAL LANGUAGE MODE)', [$q]);
+            $recipes = $recipes->where(function($query) use ($q) {
+                $query->whereRaw('MATCH(title, slug) AGAINST (? IN BOOLEAN MODE)', ["*$q*"])
+                      ->orWhere('title', 'like', "%$q%")
+                      ->orWhere('slug', 'like', "%$q%");
+            });
+            
         }
+        
         $recipes = $recipes->get();
 
         return view('frontend.recipe.index', compact('recipes'));
@@ -23,5 +29,20 @@ class RecipeController extends Controller
 
     public function show(Recipe $recipe) {
         return view('frontend.recipe.detail', compact('recipe'));
+    }
+    
+
+    public function suggest(Request $request) {
+        $query = $request->input('q');
+        
+        if(strlen($query) < 2) {
+            return response()->json([]);
+        }
+        
+        $recipes = Recipe::where('title', 'like', "$query%")
+                       ->limit(5)
+                       ->get(['id', 'title']);
+                       
+        return response()->json($recipes);
     }
 }
